15335c84803ef9ae65caa461d3d346ba26c3860f
이상윤 <sangyun3631@gmail.com>
2020-11-30 18:29:28 +0900

    Add some example

---
+++ Assets/Game/Scripts/LoadLuaByFile.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoadLuaByFile : MonoBehaviour
+{
+    private XLua.LuaEnv luaEnv = null;
+
+    void Start()
+    {
+        luaEnv = new XLua.LuaEnv();
+
+        //luaEnv.DoString("print('hello world by execute string'");
+        luaEnv.DoString("require 'LuaScripts.byfile'");
+    }
+
+    private void Update()
+    {
+        luaEnv.Tick();
+    }
+
+    private void OnDestroy()
+    {
+        luaEnv.Dispose();
+    }
+}
---
+++ Assets/Game/Scripts/xLuaTester.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using UnityEngine;
+
+public class xLuaTester : MonoBehaviour
+{
+    private void Start()
+    {
+        var luaEnv = new XLua.LuaEnv();
+        luaEnv.DoString("CS.UnityEngine.Debug.Log('hello world')");
+        luaEnv.Dispose();
+    }
+}
