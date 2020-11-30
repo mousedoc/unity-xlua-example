33e156c2f9e38d97e0c1dcf9473feb53cc981435
이상윤 <sangyun3631@gmail.com>
2020-11-30 15:14:23 +0900

    Add test case

--- Assets/Game/CSharpScripts/xLuaTester.cs
+++ Assets/Game/CSharpScripts/xLuaTester.cs
@@ -7,7 +7,14 @@ public class xLuaTester : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        XLua.LuaEnv luaenv = new XLua.LuaEnv();
+        luaenv.DoString("CS.UnityEngine.Debug.Log('hello world')");
+        luaenv.Dispose();
+    }
+
+    public static void HellWorld()
+    {
+        Debug.Log("hello world");
     }
 
     // Update is called once per frame
