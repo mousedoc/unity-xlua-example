71ab657e44d2ee45d220846ec153fc9cf840b949
이상윤 <sangyun3631@gmail.com>
2020-12-01 17:47:40 +0900

    Create lua manager

---
+++ Assets/Game/Script/Example/HelloWorld.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HelloWorld : MonoBehaviour
+{
+    private void Awake()
+    {
+        LuaManager.Initialize();
+
+
+        LuaManager.Release();
+    }
+}
---
+++ Assets/Game/Script/LuaManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class LuaManager
+{
+    public static XLua.LuaEnv Env { get; private set; } = null;
+
+    private static bool isInitialzed = false;
+
+    public static void Initialize()
+    {
+        if (isInitialzed) 
+            return;
+
+        Env = new XLua.LuaEnv();
+
+        isInitialzed = true;
+    }
+
+    public static void Release()
+    {
+        Env?.Dispose();
+        Env = null;
+    }
+}
